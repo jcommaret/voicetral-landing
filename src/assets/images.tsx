98522b37af549f8c3b6b6978c3e7ad405e87e345
Jérôme Commaret <jcommaret@gmail.com>
2024-12-08 00:50:41 +0100

    fix types

--- src/assets/images.tsx
+++ src/assets/images.tsx
@@ -1,6 +1,5 @@
 const img = {
   logo: new URL("./logo/logo.png", import.meta.url).href,
   bannerBlack: new URL("./banner/banner-black.png", import.meta.url).href,
-  bannerWhite: new URL("./banner/banner-transparent.png", import.meta.url).href,
 }
 export default img
--- src/types/nav.types.ts
+++ src/types/nav.types.ts
@@ -1,15 +1,15 @@
 export interface NavLink {
-  text: string;
-  path: string;
+  text: string
+  path: string
 }
 
 export interface NavLogo {
-  text: string;
-  path: string;
-  image: string;
+  text: string
+  path: string
+  img: string
 }
 
 export interface NavData {
-  logo: NavLogo;
-  links: NavLink[];
+  logo: NavLogo
+  links: NavLink[]
 }
