a544fd4b297ab8910f84a29f295f9d783d4b7f34
Jérôme Commaret <jcommaret@gmail.com>
2024-12-08 23:50:30 +0100

    x

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import * as path from "path"
+import { fileURLToPath } from "url"
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/tgv/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@types": path.resolve(__dirname, "./src/types"),
+    },
+  },
 })
