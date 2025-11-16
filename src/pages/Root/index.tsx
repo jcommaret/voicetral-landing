7c7c43cd74ef808a48f29a976dc377e87da823aa
Jérôme Commaret <jcommaret@gmail.com>
2025-11-16 14:54:35 +0100

    Fix import case for Footer component in Root page

--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Footer from '@components/Footer';
+import Footer from '@components/footer';
 import siteContent from '@data/content.json';
 import { SiteContent } from '../../types';
 import './index.scss';
